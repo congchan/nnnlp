b9706bf6e330adb59c00ceb65837cb6ece9c9bd7
MiaoMiao <18083731+congchan@users.noreply.github.com>
2020-07-05 17:19:01 +0800

    add contrastive_loss

--- modeling.py
+++ modeling.py
@@ -332,6 +332,14 @@ def am_softmax(logits, labels, num_classes, scale=30, margin=0.35):
     return per_token_loss, predictions  
 
 
+def contrastive_loss(model1, model2, y, margin):
+	with tf.name_scope("contrastive-loss"):
+		distance = tf.sqrt(tf.reduce_sum(tf.pow(model1 - model2, 2), 1, keepdims=True))
+		similarity = y * tf.square(distance) # keep the similar label (1) close to each other
+		dissimilarity = (1 - y) * tf.square(tf.maximum((margin - distance), 0)) # give penalty to dissimilar label if the distance is bigger than margin
+		return tf.reduce_mean(dissimilarity + similarity) / 2
+
+
 def lstm_fused(inputs, sequence_length, lstm_size, bilstm_dropout_rate, 
     is_training, num_layers=1):
   """ FusedRNNCell
